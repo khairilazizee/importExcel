62e767a313f2c7f244821dfee1ebc1aa9c6670e3
Mohamad khairil azizee bin mohamed @ ayub <khairil.mohamed@mdec.com.my>
2023-11-05 16:18:07 +0800

    add active class in navigation

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -3,7 +3,8 @@
         <div>Home</div>
         <div class="flex gap-4">
             <div>
-                <a href="/import-file" class="hover:text-blue-500 hover:font-semibold">Import</a>
+                <a href="/import-file"
+                    class="hover:text-blue-500 hover:font-semibold {{ request()->segment(1) == 'import-file' ? 'text-blue-500' : '' }}">Import</a>
             </div>
             <div>
                 <a href="/logout" class="hover:text-blue-500 hover:font-semibold">Logout</a>
